783f77e8169be7221fb5d4d250b992fed6787909
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-12-05 21:03:48 -0300

    vit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'public/dist', // build vai para /public/dist dentro do release
+    emptyOutDir: true,     // limpa o dist antigo dentro de public
   },
 })
